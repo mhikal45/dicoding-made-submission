521010040fc659b45c083170955a76d87096f608
mhikal45 <a2292218@bangkit.academy>
2021-05-27 12:08:39 +0700

    cleaning code

--- favorite/src/main/java/com/wal/winteranimelist2021/maps/FavoriteFragment.kt
+++ favorite/src/main/java/com/wal/winteranimelist2021/maps/FavoriteFragment.kt
@@ -2,10 +2,10 @@ package com.wal.winteranimelist2021.maps
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wal.winteranimelist2021.core.ui.AnimeAdapter
 import com.wal.winteranimelist2021.maps.databinding.FragmentFavoriteBinding
